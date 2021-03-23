fe62f0529d0192972524eb6d0e235cf5517d2c6b
Pierre-Louis <you@example.com>
2021-03-23 22:26:45 +0100

    Ajout de la troisième fonction de test, qui vérifie que 'Fizz' est bien retourné quand le nombre donné est un multiple de trois.

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -25,4 +25,13 @@ class FizzBuzzTest extends PHPUnit\Framework\TestCase
         $fizz = new FizzBuzz(0);
         $fizz->play();
     }
+
+    /**
+     * Check if "Fizz" is returned when a multiple of three is passed in parameter.
+     */
+    public function testFizz()
+    {
+        $fizz = new FizzBuzz(3);
+        $this->assertEquals('Fizz', $fizz->play());
+    }
 }
