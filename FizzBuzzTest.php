3a4eda57e57b209a99e331a302ac8bf59edc8ae3
Pierre-Louis <you@example.com>
2021-03-23 22:21:59 +0100

    Ajout du second test qui verifie que l'exception InvalidNumberException est bien retourné quand le nombre donné correspond à 0. Une erreur se produit dans ce test malgrès que l'égalité est bien vérifié dans la classe Fizzbuzz.. à vérifié.

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,6 +1,7 @@
 <?php
 require 'Fizzbuzz.php';
 require "InvalidNumberException.php";
+
 class FizzBuzzTest extends PHPUnit\Framework\TestCase
 {
     /**
@@ -11,6 +12,17 @@ class FizzBuzzTest extends PHPUnit\Framework\TestCase
     {
         $this->expectException(InvalidNumberException::class);
         $fizz = new FizzBuzz(-2);
+        $fizz->play();
+    }
+
+    /**
+     * Check if an exception is returned when zero is passed in parameter.
+     * @throws InvalidNumberException
+     */
+    public function testWhenEqualToZero()
+    {
+        $this->expectException(InvalidNumberException::class);
+        $fizz = new FizzBuzz(0);
         $fizz->play();
     }
 }
